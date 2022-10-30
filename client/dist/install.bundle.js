74dd04c340f25883c66d6988e38534db65efd0c4
mnwana <mariellenwana@gmail.com>
2022-10-30 17:37:30 -0400

    fix errors

--- client/dist/install.bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{var t=document.getElementById("buttonInstall");window.addEventListener("beforeinstallprompt",(function(e){e.preventDefault(),t.style.visibility="visible",t.addEventListener("click",(function(){e.prompt(),t.setAttribute("disabled",!0),t.textContent="Installed!"}))})),window.addEventListener("appinstalled",(function(t){console.log("👍","appinstalled",t)}))})();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,24 +5,16 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     butInstall.style.visibility = 'visible';
-
-    butInstall.addEventListener('click', () => {
+    // TODO: Implement a click event handler on the `butInstall` element
+    butInstall.addEventListener('click', async (event) => {
+        event.preventDefault();
         event.prompt();
         butInstall.setAttribute('disabled', true);
         butInstall.textContent = 'Installed!';
-      });
+    });
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-// butInstall.addEventListener('click', async (event) => {
-//     console.log('clicked!');
-//     event.preventDefault();
-//     event.prompt();
-//     installBtn.setAttribute('disabled', true);
-//     installBtn.textContent = 'Installed!';
-// });
+
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-    console.log('👍', 'appinstalled', event);
-});
+window.addEventListener('appinstalled', (event) => {  console.log('👍', 'appinstalled', event);});
